672d697382fee097eec5e433905067b7d64353ff
Sorami Hisamoto <s@89.io>
2023-01-01 04:11:32 +0900

    prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact(), prefetch()]
 });
